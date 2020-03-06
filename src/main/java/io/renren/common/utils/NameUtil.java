d198ec004d44ee9784a90e483cc627c11b7cde67
zhangqijie <zhangqijie_ld@qq.com>
2020-03-06 13:49:33 +0800

    修改： 1、提交驼峰转换工具

---
+++ src/main/java/io/renren/common/utils/NameUtil.java
@@ -0,0 +1,70 @@
+package io.renren.common.utils;
+
+import static io.renren.common.constant.Constant.UNDERLINE;
+
+/**
+ * 成都一方思致科技有限公司
+ *
+ * @Author intasect
+ * @Date 2020/3/6 13:40
+ * =========================================================================
+ * 变更履历：
+ * -------------------------------------------------------------------------
+ * 变更编号     变更时间    变更人   变更原因    变更内容
+ * <p>
+ * -------------------------------------------------------------------------
+ */
+public class NameUtil {
+
+    /**
+     *
+     * @param para
+     * @return
+     */
+    public static String underLineToHump(String para) {
+        StringBuilder result = new StringBuilder();
+        String a[] = para.split(UNDERLINE);
+        for (String s : a) {
+            if (!para.contains(UNDERLINE)) {
+                result.append(s);
+                continue;
+            }
+            if (result.length() == 0) {
+                result.append(s.toLowerCase());
+            } else {
+                result.append(s.substring(0, 1).toUpperCase());
+                result.append(s.substring(1).toLowerCase());
+            }
+        }
+        return result.toString();
+    }
+
+
+    /***
+     * 驼峰命名转为下划线命名
+     *
+     * @param para
+     */
+    public static String humpToUnderLine(String para) {
+        StringBuilder sb = new StringBuilder(para);
+        //定位
+        int temp = 0;
+        if (!para.contains(UNDERLINE)) {
+            for (int i = 0; i < para.length(); i++) {
+                if (i == 0){
+                    continue;
+                }
+                if (Character.isUpperCase(para.charAt(i))) {
+                    sb.insert(i + temp, UNDERLINE);
+                    temp += 1;
+                }
+            }
+        }
+        return sb.toString().toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NameUtil.humpToUnderLine("CCalalslLss"));
+        System.out.println(NameUtil.underLineToHump("calu_clau"));
+    }
+}
